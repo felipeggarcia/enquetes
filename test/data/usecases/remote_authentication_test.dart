ae39989142b912118bb5d1a67816d77f1707d652
Felipe Garcia <felipegogarcia@gmail.com>
2022-11-01 11:13:20 -0300

    move SUT to the started of the testes

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -25,10 +25,12 @@ abstract class HttpClient {
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
+  HttpClientSpy httpClient = HttpClientSpy();
+  String url = faker.internet.httpUrl();
+  RemoteAuthentication sut =
+      RemoteAuthentication(httpClient: httpClient, url: url);
+
   test('Should call HttpClient with correct values', () async {
-    final httpClient = HttpClientSpy();
-    final url = faker.internet.httpUrl();
-    final sut = RemoteAuthentication(httpClient: httpClient, url: url);
     await sut.auth();
     verify(httpClient.request(
       url: url,
