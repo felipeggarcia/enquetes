d387735445fbcd9a84ae9b930916ec87be95debc
Felipe Garcia <felipegogarcia@gmail.com>
2022-11-06 14:20:40 -0300

    ensure HttpAdapter throws NotFoundError if post returns 404

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -103,6 +103,20 @@ void main() {
 
       expect(future, throwsA(HttpError.unauthorized));
     });
+    test('Should return ForbiddenError if post returns 403', () async {
+      mockResponse(403);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.forbidden));
+    });
+    test('Should return NotFoundError if post returns 404', () async {
+      mockResponse(404);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.notFound));
+    });
     test('Should return ServerError if post returns 500', () async {
       mockResponse(500);
 
