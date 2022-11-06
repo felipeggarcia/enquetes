e9652de9d7441b1d6df785064d15b8d024d47c14
Felipe Garcia <felipegogarcia@gmail.com>
2022-11-06 14:36:46 -0300

    ensure HttpAdapter throws ServerError if post throws

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -19,8 +19,12 @@ class HttpAdapter implements HttpClient {
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
     var response = Response('', 500);
-    if (method == 'post') {
-      response = await client.post(url, headers: headers, body: jsonBody);
+    try {
+      if (method == 'post') {
+        response = await client.post(url, headers: headers, body: jsonBody);
+      }
+    } catch (_) {
+      throw HttpError.serverError;
     }
     return _handleResponse(response);
   }
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -34,6 +34,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       mockResponse(200);
     });
@@ -124,6 +128,13 @@ void main() {
     });
     test('Should return ServerError if post returns 500', () async {
       mockResponse(500);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+    test('Should return ServerError if post throws', () async {
+      mockError();
 
       final future = sut.request(url: url, method: 'post');
 
