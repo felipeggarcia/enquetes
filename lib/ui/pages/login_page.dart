6e0f93df4a03229ac36eeb3041001dcc0d0d7828
Felipe Garcia <felipegogarcia@gmail.com>
2022-11-10 20:51:10 -0300

    ensure LoginPage loads with correct initial states

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -40,7 +40,7 @@ class LoginPage extends StatelessWidget {
                     ),
                   ),
                   RaisedButton(
-                    onPressed: () {},
+                    onPressed: null,
                     child: Text('Entrar'.toUpperCase()),
                   ),
                   FlatButton.icon(
---
+++ test/ui/page/login_page_test.dart
@@ -0,0 +1,33 @@
+import 'package:enquetes/ui/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Should load with correct initial state ',
+      (WidgetTester tester) async {
+    final loginPage = MaterialApp(home: LoginPage());
+    await tester.pumpWidget(loginPage);
+
+    final emailTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
+    expect(
+      emailTextChildren,
+      findsOneWidget,
+      reason:
+          'when a TextFormField has only one text child, means its has no error, since one of the childs is always the label text',
+    );
+
+    final passwordTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
+    expect(
+      passwordTextChildren,
+      findsOneWidget,
+      reason:
+          'when a TextFormField has only one text child, means its has no error, since one of the childs is always the label text',
+    );
+
+    final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
+    expect(button.onPressed, null);
+  });
+}
