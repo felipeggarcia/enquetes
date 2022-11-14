600190b9251d16000f97a054845029825c67cd7b
Felipe Garcia <felipegogarcia@gmail.com>
2022-11-14 10:13:39 -0300

    move PasswordInput to his own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -56,23 +56,7 @@ class _LoginPageState extends State<LoginPage> {
                           top: 8,
                           bottom: 32,
                         ),
-                        child: StreamBuilder<String>(
-                            stream: widget.presenter.passwordErrorStream,
-                            builder: (context, snapshot) {
-                              return TextFormField(
-                                decoration: InputDecoration(
-                                  labelText: 'Senha',
-                                  icon: Icon(Icons.lock,
-                                      color:
-                                          Theme.of(context).primaryColorLight),
-                                  errorText: snapshot.data?.isEmpty == true
-                                      ? null
-                                      : snapshot.data,
-                                ),
-                                obscureText: true,
-                                onChanged: widget.presenter.validatePassword,
-                              );
-                            }),
+                        child: PasswordInput(),
                       ),
                       StreamBuilder<bool>(
                           stream: widget.presenter.isFormValidStream,
@@ -98,5 +82,26 @@ class _LoginPageState extends State<LoginPage> {
         );
       }),
     );
+  }
+}
+
+class PasswordInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final presenter = Provider.of<LoginPresenter>(context);
+    return StreamBuilder<String>(
+        stream: presenter.passwordErrorStream,
+        builder: (context, snapshot) {
+          return TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Senha',
+              icon:
+                  Icon(Icons.lock, color: Theme.of(context).primaryColorLight),
+              errorText: snapshot.data?.isEmpty == true ? null : snapshot.data,
+            ),
+            obscureText: true,
+            onChanged: presenter.validatePassword,
+          );
+        });
   }
 }
