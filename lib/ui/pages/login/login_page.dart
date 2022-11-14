4f762099f01b4b19dd6e2f2556c5d86d013cd706
Felipe Garcia <felipegogarcia@gmail.com>
2022-11-14 10:33:33 -0300

    move PasswordInput to his own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -84,24 +84,3 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
-
-class PasswordInput extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final presenter = Provider.of<LoginPresenter>(context);
-    return StreamBuilder<String>(
-        stream: presenter.passwordErrorStream,
-        builder: (context, snapshot) {
-          return TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Senha',
-              icon:
-                  Icon(Icons.lock, color: Theme.of(context).primaryColorLight),
-              errorText: snapshot.data?.isEmpty == true ? null : snapshot.data,
-            ),
-            obscureText: true,
-            onChanged: presenter.validatePassword,
-          );
-        });
-  }
-}
