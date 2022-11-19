fcaf251f45ef6d0fa52c288cbd164cc09e535332
Felipe Garcia <felipegogarcia@gmail.com>
2022-11-19 12:35:06 -0300

    move Validation to its own file

--- test/presentation/presenter/stream_login_presenter_test.dart
+++ test/presentation/presenter/stream_login_presenter_test.dart
@@ -1,13 +1,10 @@
 import 'dart:async';
 
+import 'package:enquetes/ui/presentation/protocols/protocols.dart';
 import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-
-abstract class Validation {
-  String validate({@required String field, @required String value});
-}
 
 class LoginState {
   String emailError;
@@ -50,11 +47,13 @@ void main() {
     email = faker.internet.email();
     mockValidation();
   });
+
   test('Should call Validation with correct email', () {
     sut.validateEmail(email);
 
     verify(validation.validate(field: 'email', value: email)).called(1);
   });
+
   test('Should emit email error if validation fails ', () {
     mockValidation(value: 'error');
     expectLater(sut.emailErrorStream, emits('error'));
