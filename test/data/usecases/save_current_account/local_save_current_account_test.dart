f6d0084207f4679378fbaf18532c0a4cd699c79d
Felipe Garcia <felipegogarcia@gmail.com>
2022-11-22 22:20:33 -0300

    ensure LocalSaveCurrentAccount throws UnespectedError if SaveSecureCacheStorageSpy throws

--- test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -1,4 +1,5 @@
 import 'package:enquetes/domain/entities/entities.dart';
+import 'package:enquetes/domain/helpers/helpers.dart';
 import 'package:enquetes/domain/usecases/usecases.dart';
 import 'package:meta/meta.dart';
 import 'package:faker/faker.dart';
@@ -10,7 +11,12 @@ class LocalSaveCurrentAccount implements SaveCurrentAccount {
 
   LocalSaveCurrentAccount({@required this.saveSecureCacheStorage});
   Future<void> save(AccountEntity account) async {
-    await saveSecureCacheStorage.saveSecure(key: 'token', value: account.token);
+    try {
+      await saveSecureCacheStorage.saveSecure(
+          key: 'token', value: account.token);
+    } catch (error) {
+      throw DomainError.unexpected;
+    }
   }
 }
 
@@ -32,5 +38,18 @@ void main() {
 
     verify(
         saveSecureCacheStorage.saveSecure(key: 'token', value: account.token));
+  });
+  test('Should throw UnespectedError if SaveSecureCacheStorage throws',
+      () async {
+    final saveSecureCacheStorage = SaveSecureCacheStorageSpy();
+    final sut =
+        LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
+    when(saveSecureCacheStorage.saveSecure(
+            key: anyNamed('key'), value: anyNamed('value')))
+        .thenThrow(Exception());
+
+    final account = AccountEntity(faker.guid.guid());
+    final future = sut.save(account);
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
