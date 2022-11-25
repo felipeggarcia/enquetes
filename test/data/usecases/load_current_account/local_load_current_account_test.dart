df4310b57a40696601b37598110f469dc4806511
Felipe Garcia <felipegogarcia@gmail.com>
2022-11-25 14:33:40 -0300

    ensure LocalLoadCurrentAccount returns an account on sucess

--- test/data/usecases/load_current_account/local_load_current_account_test.dart
+++ test/data/usecases/load_current_account/local_load_current_account_test.dart
@@ -1,19 +1,24 @@
+import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
-class LocalLoadCurrentAccount {
+import 'package:enquetes/domain/entities/entities.dart';
+import 'package:enquetes/domain/usecases/usecases.dart';
+
+class LocalLoadCurrentAccount implements LoadCurrentAccount {
   final FetchSecureCacheStorage fetchSecureCacheStorage;
 
   LocalLoadCurrentAccount({@required this.fetchSecureCacheStorage});
 
-  Future<void> load() async {
-    await fetchSecureCacheStorage.fetchSecure('token');
+  Future<AccountEntity> load() async {
+    final token = await fetchSecureCacheStorage.fetchSecure('token');
+    return AccountEntity(token);
   }
 }
 
 abstract class FetchSecureCacheStorage {
-  Future<void> fetchSecure(String key);
+  Future<String> fetchSecure(String key);
 }
 
 class FetchSecureCacheStorageSpy extends Mock
@@ -22,14 +27,25 @@ class FetchSecureCacheStorageSpy extends Mock
 void main() {
   LocalLoadCurrentAccount sut;
   FetchSecureCacheStorageSpy fetchSecureCacheStorage;
+  String token;
+  void mockFetchSecure() {
+    when(fetchSecureCacheStorage.fetchSecure(any))
+        .thenAnswer((_) async => token);
+  }
 
   setUp(() {
     fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
     sut = LocalLoadCurrentAccount(
         fetchSecureCacheStorage: fetchSecureCacheStorage);
+    token = faker.guid.guid();
+    mockFetchSecure();
   });
-  test('Should callFetchSecureCacheStorage with correct value', () async {
+  test('Should call FetchSecureCacheStorage with correct value', () async {
     await sut.load();
     verify(fetchSecureCacheStorage.fetchSecure('token'));
+  });
+  test('Should return an AccountEntity', () async {
+    final account = await sut.load();
+    expect(account, AccountEntity(token));
   });
 }
