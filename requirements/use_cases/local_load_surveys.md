e9c5a140f313dcf9b2160c014553d6016d08db54
Felipe Garcia <felipegogarcia@gmail.com>
2022-12-07 14:57:36 -0300

    ensure RemoteLoadSurveys calls HttpClient with correct values

--- requirements/use_cases/local_load_surveys.md
+++ requirements/use_cases/local_load_surveys.md
@@ -1,14 +1,14 @@
 # Local Load Current Account Use Case
 
 > ## Caso de sucesso
-1. Sistema solicita os dados das enquetes do cache
-2. Sistema valida os dados recebidos do cache
+1. Sistema solicita os dados das enquetes do Cache
+2. Sistema valida os dados recebidos do Cache
 3. Sistema entrega os dados das enquetes
 
 > ## Exceção - Cache vazio
 1. Sistema retorna uma mensagem de erro inesperado
 
 
-> ## Exceção - Dados inválidos no cache
+> ## Exceção - Dados inválidos no Cache
 1. Sistema retorna uma mensagem de erro inesperado
-2. Sistema limpa os dados do cache
+2. Sistema limpa os dados do Cache
---
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -0,0 +1,29 @@
+import 'package:enquetes/data/http/http.dart';
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+class RemoteLoadSurveys {
+  final String url;
+  final HttpClient httpClient;
+  RemoteLoadSurveys({@required this.url, @required this.httpClient});
+
+  Future<void> load() async {
+    await httpClient.request(url: url, method: 'get');
+  }
+}
+
+class HttpClientSpy extends Mock implements HttpClient {}
+
+void main() {
+  test('Should call httpClient with correct values', () async {
+    final url = faker.internet.httpUrl();
+    final httpClient = HttpClientSpy();
+    final sut = RemoteLoadSurveys(url: url, httpClient: httpClient);
+
+    await sut.load();
+
+    verify(httpClient.request(url: url, method: 'get'));
+  });
+}
