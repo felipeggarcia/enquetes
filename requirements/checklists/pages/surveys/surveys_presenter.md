27a93e2b4fffebdd8dffce52375382d3c1fffb77
Felipe Garcia <felipegogarcia@gmail.com>
2022-12-09 16:46:46 -0300

    move GetxSurveysPresenter to its own file

--- requirements/checklists/pages/surveys/surveys_presenter.md
+++ requirements/checklists/pages/surveys/surveys_presenter.md
@@ -2,8 +2,8 @@
 
 > ## Regras
 1. ✅Chamar o LoadSurveys no método loadData 
-2. Notificar o isLoadingStream como true antes de chamar o LoadSurveys
-3. Notificar o isLoadingStream como false no fim do LoadSurveys
-4. Notificar o surveysStream como erro caso LoadSurveys retorne erro
-5. Notificar o surveysStream como uma lista de Surveys caso o LoadSurveys retorne sucesso
+2. ✅Notificar o isLoadingStream como true antes de chamar o LoadSurveys
+3. ✅Notificar o isLoadingStream como false no fim do LoadSurveys
+4. ✅Notificar o surveysStream como erro caso LoadSurveys retorne erro
+5. ✅Notificar o surveysStream como uma lista de Surveys caso o LoadSurveys retorne sucesso
 6. Levar o usuário para a tela de Resultado da Enquete ao clicar em alguma enquete
--- test/presentation/presenters/getx_surveys_presenter_test.dart
+++ test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -1,51 +1,20 @@
 import 'package:enquetes/domain/helpers/domain_error.dart';
+import 'package:enquetes/presentation/presenters/presenters.dart';
 import 'package:enquetes/ui/helpers/errors/errors.dart';
 import 'package:enquetes/ui/pages/pages.dart';
 import 'package:faker/faker.dart';
-import 'package:intl/intl.dart';
+
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
-import 'package:get/get.dart';
 
 import 'package:enquetes/domain/entities/entities.dart';
 import 'package:enquetes/domain/usecases/load_surveys.dart';
-
-class GetxSurveysPresenter {
-  final LoadSurveys loadSurveys;
-
-  final _isLoading = true.obs;
-  final _surveys = Rx<List<SurveyViewModel>>();
-
-  Stream<bool> get isLoadingStream => _isLoading.stream;
-  Stream<List<SurveyViewModel>> get surveysStream => _surveys.stream;
-
-  GetxSurveysPresenter({@required this.loadSurveys});
-  Future<void> loadData() async {
-    try {
-      _isLoading.value = true;
-      final surveys = await loadSurveys.load();
-      _surveys.value = surveys
-          .map((survey) => SurveyViewModel(
-              id: survey.id,
-              question: survey.question,
-              date: DateFormat('dd MMM yyyy').format(survey.dateTime),
-              didAnswer: survey.didAnswer))
-          .toList();
-    } on DomainError {
-      _surveys.subject.addError(UIError.unexpected.description);
-    } finally {
-      _isLoading.value = false;
-    }
-  }
-}
 
 class LoadSurveysSpy extends Mock implements LoadSurveys {}
 
 void main() {
   LoadSurveysSpy loadSurveys;
   GetxSurveysPresenter sut;
-  List<SurveyEntity> surveys;
 
   List<SurveyEntity> mockValidData() => [
         SurveyEntity(
@@ -62,7 +31,6 @@ void main() {
   PostExpectation mockLoadSurveysCall() => when(loadSurveys.load());
 
   void mockLoadSurveys(List<SurveyEntity> data) {
-    surveys = data;
     mockLoadSurveysCall().thenAnswer((_) async => data);
   }
 
